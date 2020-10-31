9f6f96c7758250862c97e36df6953b64320b9451
HabibuGG <byjavimc@gmail.com>
2020-10-31 15:42:09 +0100

    Actualizada página para el tercer hito

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
 
 document.addEventListener("DOMContentLoaded", function(event) {
   var scrollButton = document.getElementById("boton-scroll");
-  var alertButton = document.getElementById("boton-alerta");
+  var searchField = document.getElementById("input-buscador");
+  var searchButton = document.getElementById("buscador");
+  var image = document.getElementById("meme-imagen");
+  var stars = document.getElementById("estrellas");
+  var url = "https://api.github.com/users/HabibuGG/repos";
+  var memeURL = "https://http.cat/"
 
   window.onscroll = function() {scrollFunction()};
 
@@ -20,8 +25,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
     window.scrollTo(0, 0);
   });
 
-  alertButton.addEventListener("click", (e) => {
-    alert("Bienvenido al segundo hito!")
+  
+  searchButton.addEventListener("click", (e) => {
+    let text = searchField.value;
+    if(text.trim().length > 0){
+      image.src = memeURL + text;
+      image.style.display = "block";
+    }
+  });
+
+  $.get(url, (data, status) => {
+    if(status != "success") return;
+    for(let i = 0; i < data.length; i++){
+      if(data[i].name == "core-academy"){
+        stars.innerHTML = data[i].stargazers_count;
+        break;
+      }
+    }
   });
 
 });
